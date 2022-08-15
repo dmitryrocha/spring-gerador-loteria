5ee85651b9188bc033f9c698944abcbaa24f6ec7
Dmitry Rocha <dmrosilva@tjba.jus.br>
2022-08-15 11:24:49 -0300

    Initial commit

---
+++ src/main/java/com/recosoft/geradorloteria/GeradorLoteriaApplication.java
@@ -0,0 +1,13 @@
+package com.recosoft.geradorloteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorLoteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeradorLoteriaApplication.class, args);
+    }
+
+}
